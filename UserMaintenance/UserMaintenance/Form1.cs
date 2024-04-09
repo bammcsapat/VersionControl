f9f9eabc91c0e7637e0bfc9fd265ba5c90ee2ecb
Amrich Csongor <csongor.amrich@stud.uni-corvinus.hu>
2024-04-09 14:51:03 +0200

    Teszt

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -6,5 +6,10 @@ namespace UserMaintenance
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Teszt");
+        }
     }
 }
